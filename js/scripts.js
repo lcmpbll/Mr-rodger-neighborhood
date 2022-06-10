e57e220b7e826e05053d5607a7f7aa9bba32defd
Liam Campbell <liam22campbell@gmail.com>
2022-06-10 16:43:12 -0700

    WIP add jquery and css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,54 @@
 
+	//business logic
+	
 	function beepBoop(countTo){
 		let array= [];
 		let array1 = [1];
 		let array2 = [2];
 		let array3 = [3];
-		let neighborArray = [];
+		let arrayNeighbor = []
+		let arrayBoop = []
+		let finalArray = [];
 		for(index = 0; index <= countTo; index +=1){
 			array.push(index);
 			}
 			for (let index = 0; index < array.length; index ++) {
 				if (array3.includes(array)) {
-					let neighborArray = array.splice(index, 1, "won't you be my neighbor");
-				break;
+					let arrayFirstSlice = array.slice(0, index -1)
+					let arrayLastSlice = array.slice(index +1, array.length);
+						arrayNeighbor.push(arrayFirstSlice.join(",") + "Won't you be my neighbor" + arrayLastSlice.join(""));
+				
 			}
+			if (array2.includes(arrayNeighbor)) {
+				let arrayFirstSlice = arrayNeighbor.slice(0, index -1);
+				let arrayLastSlice = arrayNeighbor.slice(index +1, arrayNeighbor.length);
+				arrayBoop.push(arrayFirstSlice.join(",") + "Boop!" + arrayLastSlice.join(""));
 			}
+			if (array1.includes(arrayBoop)) {
+				let arrayFirstSlice = arrayBoop.slice(0, index -1);
+				let arrayLastSlice = arrayBoop.slice(index +1, arrayBoop.length);
+				finalArray.push(arrayFirstSlice.join(",") + "Beep!" + arrayLastSlice.join(""));
+			}
+			return finalArray;
+
+			}
+
 			
 			
 	}
+ 
+	//UI logic
+$(doucment).ready(function(){
+	$(#formOne).submit(function(event){
+		event.preventDefault;
+		$($results).show();
+		const countTo = parseInt($("#countingTo").val());
+		const results = beepBoop(countTo);
+		$("#results").append(results+"<br>");
+	});
+});
+
+
 
 
 
