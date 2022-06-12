ffb19cff9bb828b7d9adcb2bb6f92f61c15d7ff2
Liam Campbell <liam22campbell@gmail.com>
2022-06-12 16:32:16 -0700

    Add picture and styling

--- README.md
+++ README.md
@@ -20,20 +20,30 @@
 Code: beepBoop(3);
 Expected output: [0, 1, 2, 3]
 
+
 Test : "It should Identify the number 3 in the array."
 Code: beepBoop(4);
 Expected output: [0 false, 1, false, 2, false, 3, true, 4, false]
 
 Test "It should identify the number three in two didgit numbers"
 code: beepBoop (13);
-Expected output:
+Expected output: [ [0 false, 1, false, 2, false, 3, true, 4, false , 5, false, 6, false, 7, false, 8, false, 9, false, 10, false, 11, false, 12, false, 13, true ]
 
 Test : "It should replace the number 3 with "Won't you be my neighbor."
-Code: beepBoop (3);
+Code: beepBoop (5);
+Expected output:[0, 1, 2, "Won't you be my neighbor?", 4, 5]
 
-Test: "It should return the "boop" when a 1 is present in the index
-Code: beepBoop (2);
-Expected output: ["boop", 2]
+Test: "It should replace the number 2 with "Boop!".
+Code: beepBoop(2);
+Expected output: [0, 1, "Boop!"]
+
+Test: "It should replace the number 1 with "Beep!".
+Code: beepBoop(1);
+Expected output: [0, "Beep!"]
+
+Test: "It should run replacements in 3>2>1 order of importance."
+Code: beepBoop(15);
+Expected output: [0, 'Beep!', 'Boop!', "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, 'Beep!', 'Beep!', 'Boop!', "Won't you be my neighbor?", 'Beep!', 'Beep!']
 
 _This application takes the number that the user inputs and uses JavaScript to output a string of numbers from 0 to the output substituting "beeps" on every number that contains a one, "boops" on every number that contains a two, and "Won't you be my neighbor" on every number that contains a three._
 
--- js/scripts.js
+++ js/scripts.js
@@ -3,47 +3,20 @@
 	
 function beepBoop(countTo){
 		let array= [];
-	
-	for(index = 0; index <= countTo; index +=1){
-			
-			let x = [index].toString();
+	for(index = 0; index <= countTo; index ++) {
+			let x = index.toString();
 			if (x.includes("3")) {
 				array.push("Won't you be my neighbor?");
-			// } else if (x.includes("2")) {
-			// 	array.push("Boop!");
-			
-			}else {
-				array.push([index])
+			} else if (x.includes("2")) {
+		 	array.push("Boop!");
+			} else if (x.includes("1")) {
+			array.push("Beep!");	
+			} else {
+				array.push(index);
 			}
-			console.log(array);
-			return array;
-			
-			// for (let index = 0; index < array.length; index ++) {
-			
-				// let arrayFirstSlice = array.slice(0, index -1)
-				// let arrayLastSlice = array.slice(index +1, array.length);
-				// arrayNeighbor.push(arrayFirstSlice.join(",") + "Won't you be my neighbor" + arrayLastSlice.join(""));
-				
 		}
+			return array;
 	}
-			// if (array2.includes(arrayNeighbor)) {
-			// 	let arrayFirstSlice = arrayNeighbor.slice(0, index -1);
-			// 	let arrayLastSlice = arrayNeighbor.slice(index +1, arrayNeighbor.length);
-			// 	arrayBoop.push(arrayFirstSlice.join(",") + "Boop!" + arrayLastSlice.join(""));
-			// }
-			// if (array1.includes(arrayBoop)) {
-			// 	let arrayFirstSlice = arrayBoop.slice(0, index -1);
-			// 	let arrayLastSlice = arrayBoop.slice(index +1, arrayBoop.length);
-			// 	finalArray.push(arrayFirstSlice.join(",") + "Beep!" + arrayLastSlice.join(""));
-			// }
-		
-
-
-
-			
-			
-	
- 
 	//UI logic
 $(document).ready(function(){
 	$("#formOne").submit(function(event){
@@ -51,10 +24,9 @@ $(document).ready(function(){
 		$("#results").show();
 		const countTo = parseInt($("#countingTo").val());
 		const results = beepBoop(countTo);
-		$("#results").append(results+"<br>");
+		$("#results").text(results);
 	});
 });
-
 
 
 
